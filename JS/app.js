dd6cd2f09b5a22b9d32b3be290d1ea82b19b64f4
Oceane BROSSEAU <oceane.brosseau2024@campus-eni.fr>
2024-10-22 17:09:50 +0200

    maj début du memory

--- JS/app.js
+++ JS/app.js
@@ -1,36 +1,114 @@
-
-
-//création du memory (tableau avec les cartes)
+//creer le tableau de jeu avec les cartes
 const cardsArray = [
   {
-    name: "gorille",
-    img: "./img/animaux/1.webp",
+    name: "crocodile",
+    img: "JS/img/animaux/5.webp",
   },
   {
-    name: "zebu",
-    img: "./img/animaux/2.webp",
-  },
-
-  {
-    name: "phacochere",
-    img: "./img/animaux/3.webp",
+    name: "lion",
+    img: "JS/img/animaux/9.webp",
   },
   {
-    name: "chimpanze",
-    img: "./img/animaux/4.webp",
+    name: "elephant",
+    img: "JS/img/animaux/6.webp",
+  },
+  {
+    name: "wolf",
+    img: "JS/img/animaux/25.webp",
   },
 ];
 
-const game = document.getElementById('game');
-const grid = document.createElement.apply('section');
-grid.setAttribute('class', 'grid')
+//attraper un élément pour lui ajouter celui dont on a besoin
+const game = document.getElementById("game");
+const grid = document.createElement("section");
+grid.setAttribute("class", "grid");
+game.appendChild(grid);
 
-game.appendChild(grid)
+//grille de jeu cartes x2
+const gameGrid = cardsArray.concat(cardsArray);
 
-cardsArray.forEach((item)=>{
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.name = item.name;
-    card.style.backgroundImage = 'url(${item.img})'
-    grid.appendChild(card)
-})
+//rendre aléatoire la grille de jeu
+gameGrid.sort(() => 0.5 - Math.random());
+
+let firstTry = "";
+let secondTry = "";
+let counter = 0;
+//let delay = 1000; //en ms
+
+//à partir du tableau de jeu créer chaque élément correspondant en HTML
+gameGrid.forEach((cardElement) => {
+  const card = document.createElement("div");
+  //ajouter l'image dans l'element
+  card.classList.add("card");
+  card.dataset.name = cardElement.name;
+  //créer une face avant
+  const frontCard = document.createElement("div");
+  frontCard.classList.add("frontCard");
+  //créer une face arrière
+  const backCard = document.createElement("div");
+  backCard.classList.add("backCard");
+  backCard.style.backgroundImage = `url(${cardElement.img})`;
+  //la carte à la grille + carte avec face avant + face arrière
+  grid.appendChild(card);
+  card.appendChild(frontCard);
+  card.appendChild(backCard);
+});
+
+//fonction pour checker les élements qui vont de paires via la class selected CSS
+const match = () => {
+  let selected = document.querySelectorAll(".selected");
+  selected.forEach((card) => {
+    card.classList.add("match");
+  });
+};
+
+let previousTarget = null;
+//fontion pour reset les try après avoir effectuer les 2 essais
+const resetTry = () => {
+  firstTry = "";
+  secondTry = "";
+  counter = 0;
+
+  let selected = document.querySelectorAll(".selected");
+  selected.forEach((card) => {
+    card.classList.remove("selected");
+  });
+};
+
+//ajouter un evenement à chaque clic sur une carte
+grid.addEventListener("click", function (event) {
+  //cibler l'evenement du clic
+  const clicked = event.target;
+  //seul les cartes sont cliquables dans la grille + 2 par 2 pas plus
+
+  if (
+    clicked.nodeName === "SECTION" ||
+    clicked === previousTarget ||
+    clicked.parentNode.classList.contains("selected") ||
+    clicked.parentNode.classList.contains('match')
+  ) {
+    return;
+  }
+  if (counter < 2) {
+    counter++;
+    if (counter === 1) {
+      firstTry = clicked.parentNode.dataset.name;
+      console.log(firstTry);
+      clicked.parentNode.classList.add("selected");
+    } else {
+      secondTry = clicked.parentNode.dataset.name;
+      console.log(secondTry);
+      clicked.parentNode.classList.add("selected");
+    } //si les deux essais sont nuls
+    if (firstTry !== "" && secondTry !== "") {
+      //et si c'est ok
+      if (firstTry === secondTry) {
+        match();
+        resetTry();
+      } else {
+        resetTry();
+      }
+    }
+    previousTarget = clicked;
+  }
+});
